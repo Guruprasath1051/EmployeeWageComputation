2508f446b9b97290d91929cf9a9246861cb7d00d
guruprasath <guruprasath1051@gmail.com>
2023-01-20 19:44:54 +0530

    [GURU] Modified Calculating Employee Attedence

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,7 +10,20 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
+            int isPresent = 1;
             Console.WriteLine("Welcome To EmployeeWage Computation");
+
+            //UC1 - EmployeeAttendence
+            Random random = new Random();
+            int empAttendence = random.Next(0,2); //0 or 1
+            if (empAttendence == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
