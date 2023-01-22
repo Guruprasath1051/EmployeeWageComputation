b28ca0dae109160dead988744e18a92ac31f5a12
guruprasath <guruprasath1051@gmail.com>
2023-01-22 20:32:17 +0530

    [GURU] Modified Calculating Employee

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,22 +10,26 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            const int isPresent = 1;
-            int empHrs = 0;
-            const int empRatePerHr =20
-            Console.WriteLine("Welcome To EmployeeWage Computation");
+            const int FULL_TIME  = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0, empWage = 0;
 
+            Console.WriteLine("Welcome To EmployeeWage Computation");
             //UC1 - EmployeeAttendence
             Random random = new Random();
             int empAttendence = random.Next(0,2); //0 or 1
-            if (empAttendence == isPresent)
+            if (empAttendence == FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage:{0}", empWage);
             Console.ReadLine();
         }
     }
